1bcddd1924fb4717f0a2c6d0c68bc0ebb824029d
Aaron Thompson <aarontomo@protonmail.com>
2021-04-23 15:45:26 +0100

    This impliments the basic login screen. I have tested this with the Toast function. the next step is to get a splashscreen and to allow the user to change screen is the credentials are correct.

--- app/src/main/java/uk/ac/tees/m2163843/withinpasswords/MainActivity.java
+++ app/src/main/java/uk/ac/tees/m2163843/withinpasswords/MainActivity.java
@@ -3,12 +3,36 @@ package uk.ac.tees.m2163843.withinpasswords;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button)findViewById(R.id.loginButton);
+        username = (EditText)findViewById(R.id.usernameLogin);
+        password = (EditText)findViewById(R.id.userPassword);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("password")){
+                    Toast.makeText(MainActivity.this, "Correct!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Nope!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+
 }
