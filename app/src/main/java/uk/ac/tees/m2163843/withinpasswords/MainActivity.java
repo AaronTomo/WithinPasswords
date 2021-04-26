e94422faba0af4eb56007d61240185fdf347a32c
Aaron Thompson <aarontomo@protonmail.com>
2021-04-26 22:36:20 +0100

    Added a second screen with a list view. Login screen is now linked to the second screen. Array used to test the list

--- app/src/main/java/uk/ac/tees/m2163843/withinpasswords/MainActivity.java
+++ app/src/main/java/uk/ac/tees/m2163843/withinpasswords/MainActivity.java
@@ -2,6 +2,7 @@ package uk.ac.tees.m2163843.withinpasswords;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("password")){
                     Toast.makeText(MainActivity.this, "Correct!!", Toast.LENGTH_SHORT).show();
+                    Intent nextScreen = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(nextScreen);
+
+
                 } else {
                     Toast.makeText(MainActivity.this, "Nope!!", Toast.LENGTH_SHORT).show();
                 }
---
+++ app/src/main/java/uk/ac/tees/m2163843/withinpasswords/MainActivity2.java
@@ -0,0 +1,32 @@
+package uk.ac.tees.m2163843.withinpasswords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ListView passwordListView = findViewById(R.id.passwordList);
+
+        ArrayList<String> accountList = new ArrayList<String>();
+
+        accountList.add("Aaron");
+        accountList.add("Likliklik");
+
+        //this converts the array into the listview for the app to display.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, accountList);
+
+        //This takes the converted array and places into a list.
+        passwordListView.setAdapter(arrayAdapter);
+
+    }
+}
