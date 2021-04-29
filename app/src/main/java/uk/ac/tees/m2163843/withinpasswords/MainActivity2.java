ea6be0131620bca936f0d78652f97e65164a843c
Aaron Thompson <aarontomo@protonmail.com>
2021-04-29 19:19:48 +0100

    This update adds a navigation bar to the account menu and hold 3 framents. Account, Maps and settings.

--- app/src/main/java/uk/ac/tees/m2163843/withinpasswords/MainActivity2.java
+++ app/src/main/java/uk/ac/tees/m2163843/withinpasswords/MainActivity2.java
@@ -1,13 +1,18 @@
 package uk.ac.tees.m2163843.withinpasswords;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -19,18 +24,58 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        ListView passwordListView = findViewById(R.id.passwordList);
+        //ListView passwordListView = findViewById(R.id.passwordList);
 
-        ArrayList<String> accountList = new ArrayList<String>();
+        //ArrayList<String> accountList = new ArrayList<String>();
 
-        accountList.add("Aaron");
-        accountList.add("Likliklik");
+        //accountList.add("Aaron");
+        //accountList.add("Likliklik");
 
         //this converts the array into the listview for the app to display.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, accountList);
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, accountList);
 
         //This takes the converted array and places into a list.
-        passwordListView.setAdapter(arrayAdapter);
+        //passwordListView.setAdapter(arrayAdapter);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+
+        //sets the default fragment
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, new HomeFragment()).commit();
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+
+                case R.id.itemFragment:
+                    selectedFragment = new ItemFragment();
+                    break;
+
+                case R.id.mapsFragment:
+                    selectedFragment = new MapsFragmentV2();
+                    break;
+
+                case R.id.settingsFragment:
+                    selectedFragment = new SettingsFragment();
+                    break;
+
+
+            }
+
+            //Change fragment
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, selectedFragment).commit();
+
+            return true;
+        }
+    };
+
+
+
 }
---
+++ app/src/main/java/uk/ac/tees/m2163843/withinpasswords/SettingsFragment.java
@@ -0,0 +1,13 @@
+package uk.ac.tees.m2163843.withinpasswords;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
